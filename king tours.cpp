04ea6da9e09ad5cf69fb9081daabb304a01fb174
pavansuryateja60-afk <pavansuryateja60@gmail.com>
2025-12-02 20:47:10 +0530

    Add files via upload

---
+++ king tours.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+
+    int total = (N * 5) + (M * 7);
+
+    printf("%d", total);
+
+    return 0;
+}
---
+++ print the color name.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char ch;
+    scanf("%c", &ch);
+
+    ch = toupper(ch);  // Convert to uppercase
+
+    switch (ch) {
+        case 'V':
+            printf("Violet");
+            break;
+        case 'I':
+            printf("Indigo");
+            break;
+        case 'B':
+            printf("Blue");
+            break;
+        case 'G':
+            printf("Green");
+            break;
+        case 'Y':
+            printf("Yellow");
+            break;
+        case 'O':
+            printf("Orange");
+            break;
+        case 'R':
+            printf("Red");
+            break;
+        default:
+            printf("-1");
+    }
+
+    return 0;
+}
